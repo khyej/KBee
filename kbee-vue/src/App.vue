1b4415e8a479b3e2519598210c9d169f7c93a1e9
min4034415 <min4034415@gmail.com>
2025-04-07 11:11:24 +0900

    basic

--- kbee-vue/src/App.vue
+++ kbee-vue/src/App.vue
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-<script setup>
-import { RouterLink, RouterView } from 'vue-router';
-import HelloWorld from './components/HelloWorld.vue';
-</script>
-
-<template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-=======
 <template>
   <header>
     <nav>
@@ -34,7 +9,6 @@ import HelloWorld from './components/HelloWorld.vue';
       <RouterLink to="/login">Login</RouterLink>
       <RouterLink to="/logout">Logout</RouterLink>
     </nav>
->>>>>>> changyoung
   </header>
 
   <RouterView />
