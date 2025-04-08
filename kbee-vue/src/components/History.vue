08bcfc25d4ab6d30dc72dd6e99840cbeea9c2d79
khyej <wl159753@naver.com>
2025-04-08 10:11:29 +0900

    fix

--- kbee-vue/src/components/History.vue
+++ kbee-vue/src/components/History.vue
@@ -1,5 +1,12 @@
 <template>
-    <div class="h-40 w-40 bg-blue-600">거래내역.</div>
+    <div>거래내역.</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+div {
+    width: 100%;
+    height: 100%;
+    margin: 5px;
+    background-color: black;
+}
+</style>
--- kbee-vue/src/views/HistoryView.vue
+++ kbee-vue/src/views/HistoryView.vue
@@ -1,5 +1,8 @@
 <template>
-    <div class="p-4">
+    <div>
+        <History />
+    </div>
+    <!-- <div class="p-4">
         <h2 class="text-2xl font-bold mb-4">📋 수입 / 지출 내역</h2>
 
         <div class="flex space-x-2 mb-6">
@@ -20,38 +23,39 @@
                 <p>🏷️ {{ item.category }} / 💳 {{ item.payment_method || '---' }}</p>
             </li>
         </ul>
-    </div>
+    </div> -->
 </template>
 
 <script setup>
-import { ref, computed, onMounted } from 'vue'
-import axios from 'axios'
+import History from '@/components/History.vue';
+// import { ref, computed, onMounted } from 'vue'
+// import axios from 'axios'
 
-const mode = ref('all')
-const allList = ref([])
+// const mode = ref('all')
+// const allList = ref([])
 
-const fetchData = async () => {
-    const [incomesRes, expensesRes] = await Promise.all([
-        axios.get('http://localhost:3001/incomes?user_id=1'),
-        axios.get('http://localhost:3001/expenses?user_id=1'),
-    ])
+// const fetchData = async () => {
+//     const [incomesRes, expensesRes] = await Promise.all([
+//         axios.get('http://localhost:3001/incomes?user_id=1'),
+//         axios.get('http://localhost:3001/expenses?user_id=1'),
+//     ])
 
-    const incomes = incomesRes.data.map(item => ({ ...item, type: 'income' }))
-    const expenses = expensesRes.data.map(item => ({ ...item, type: 'expense' }))
+//     const incomes = incomesRes.data.map(item => ({ ...item, type: 'income' }))
+//     const expenses = expensesRes.data.map(item => ({ ...item, type: 'expense' }))
 
-    allList.value = [...incomes, ...expenses].sort((a, b) => a.id - b.id)
-}
+//     allList.value = [...incomes, ...expenses].sort((a, b) => a.id - b.id)
+// }
 
-const filteredList = computed(() => {
-    if (mode.value === 'all') return allList.value
-    return allList.value.filter(item => item.type === mode.value)
-})
+// const filteredList = computed(() => {
+//     if (mode.value === 'all') return allList.value
+//     return allList.value.filter(item => item.type === mode.value)
+// })
 
-const btnClass = (type) => {
-    return mode.value === type
-        ? 'bg-blue-500 text-white px-4 py-2 rounded'
-        : 'bg-gray-200 text-black px-4 py-2 rounded'
-}
+// const btnClass = (type) => {
+//     return mode.value === type
+//         ? 'bg-blue-500 text-white px-4 py-2 rounded'
+//         : 'bg-gray-200 text-black px-4 py-2 rounded'
+// }
 
-onMounted(fetchData)
+// onMounted(fetchData)
 </script>
