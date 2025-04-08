4ad9d770c85c89a15e795145d42850dd086bcf93
min4034415 <min4034415@gmail.com>
2025-04-08 10:08:44 +0900

    hiya

--- kbee-vue/src/App.vue
+++ kbee-vue/src/App.vue
@@ -2,46 +2,21 @@
 import Header from './components/Header.vue';
 import Sidebar from './components/Sidebar.vue';
 import { RouterView } from 'vue-router';
+import Layout from './components/Layout.vue';
 </script>
 
 <template>
-  <div class="layout">
-    <Header />
-    <div class="contentBox">
+  <Layout>
+    <template #header>
+      <Header />
+    </template>
+    <template #sidebar>
       <Sidebar />
-      <main class="mainBox">
-        <RouterView />
-      </main>
+    </template>
+    <div class="contentBox">
+      <RouterView />
     </div>
-  </div>
+  </Layout>
 </template>
 
-<style>
-@font-face {
-  font-family: 'S-CoreDream-3Light';
-  src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-3Light.woff')
-    format('woff');
-  font-weight: normal;
-  font-style: normal;
-}
-
-* {
-  font-family: 'S-CoreDream-3Light';
-}
-.layout {
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-}
-
-.contentBox {
-  display: flex;
-  flex: 1;
-}
-
-.mainBox {
-  flex: 1;
-  padding: 5px;
-  box-sizing: border-box;
-}
-</style>
+<style></style>
---
+++ kbee-vue/src/components/Layout.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="layout">
+    <slot name="header" />
+    <div class="contentBox">
+      <slot name="sidebar" />
+      <main class="mainBox"><slot /></main>
+    </div>
+  </div>
+</template>
+<script setup></script>
+<style scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.contentBox {
+  display: flex;
+  flex: 1;
+}
+
+.mainBox {
+  flex: 1;
+  padding: 5px;
+  box-sizing: border-box;
+}
+</style>
