477c89ae448386276a3d15955a044b2d3b13b2ba
Roa Kim <roa5108@gmail.com>
2025-04-10 13:34:05 +0900

    home.vue scroll remove

--- kbee-vue/src/App.vue
+++ kbee-vue/src/App.vue
@@ -10,7 +10,7 @@ const route = useRoute();
 const userStore = useUserStore();
 
 onMounted(() => {
-    userStore.restoreUser();
+  userStore.restoreUser();
 });
 
 // 헤더, 사이드바를 제외할 경로 리스트
@@ -18,55 +18,62 @@ const noLayoutPages = ['/', '/login', '/signup'];
 </script>
 
 <template>
-    <div class="layout">
-        <Header v-if="!noLayoutPages.includes(route.path)" />
-        <div class="contentBox">
-            <Sidebar v-if="!noLayoutPages.includes(route.path)" />
-            <main class="mainBox" :class="{ fullPage: noLayoutPages.includes(route.path) }">
-                <RouterView />
-            </main>
-        </div>
+  <div class="layout">
+    <Header v-if="!noLayoutPages.includes(route.path)" />
+    <div class="contentBox">
+      <Sidebar v-if="!noLayoutPages.includes(route.path)" />
+      <main
+        class="mainBox"
+        :class="{ fullPage: noLayoutPages.includes(route.path) }"
+      >
+        <RouterView />
+      </main>
     </div>
+  </div>
 </template>
 
 <style>
 @font-face {
-    font-family: 'S-CoreDream-3Light';
-    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-3Light.woff') format('woff');
-    font-weight: normal;
-    font-style: normal;
+  font-family: 'S-CoreDream-3Light';
+  src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-3Light.woff')
+    format('woff');
+  font-weight: normal;
+  font-style: normal;
 }
 
 * {
-    font-family: 'S-CoreDream-3Light';
+  font-family: 'S-CoreDream-3Light';
 }
 
 .layout {
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
 }
 
 .contentBox {
-    display: flex;
-    /* flex: 1; */
+  display: flex;
+  /* flex: 1; */
 }
 
 .mainBox {
-    margin-top: 50px;
-    /* 헤더 높이 */
-    margin-left: 160px;
-    /* 사이드바 너비 */
-    width: calc(100% - 160px);
-    height: calc(100vh - 50px);
-    overflow-y: auto;
-    padding: 20px;
-    background-color: #f0f4f9;
+  margin-top: 50px;
+  /* 헤더 높이 */
+  margin-left: 160px;
+  /* 사이드바 너비 */
+  width: calc(100% - 160px);
+  height: calc(100vh - 50px);
+  /* overflow-y: auto; */
+  overflow: hidden;
+  padding: 20px;
+  background-color: #f0f4f9;
 }
 
 .fullPage {
-    margin: 0;
-    width: 100%;
-    height: 100vh;
+  margin: 0 !important;
+  padding: 0 !important;
+  width: 100vw !important;
+  height: 100vh !important;
+  background-color: white !important;
 }
 </style>
