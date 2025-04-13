6d7160b3747844b1574df45f235d82a0e21655cc
min4034415 <65104668+min4034415@users.noreply.github.com>
2025-04-14 00:55:12 +0900

    리로딩 이슈 고침여

--- kbee-vue/vite.config.js
+++ kbee-vue/vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path'; 
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -21,5 +22,14 @@ export default defineConfig({
                 rewrite: (path) => path.replace(/^\/api/, ''),
             },
         },
+        watch: {
+            // Ignore the db.json file
+            ignored: [
+              // Use an absolute path or a relative path from the project root
+              path.resolve(__dirname, 'src/db.json'),
+              // Or if db.json is in the root:
+              // '**/db.json'
+            ],
+          },
     },
 });
